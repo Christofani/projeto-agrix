5f7ea7d52a3bc01406ea88a264f288acc26157d9
Rodrigo Christofani <christofanir@gmail.com>
2024-12-07 03:28:14 -0300

    Camada serviçe da entidade Farm, OK.

--- src/main/java/com/betrybe/agrix/services/FarmService.java
+++ src/main/java/com/betrybe/agrix/services/FarmService.java
@@ -1,4 +1,50 @@
 package com.betrybe.agrix.services;
 
+import com.betrybe.agrix.entity.*;
+import com.betrybe.agrix.exception.FarmNotFoundException;
+import com.betrybe.agrix.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.*;
+
+import java.util.*;
+
+@Service
 public class FarmService {
+
+  private final FarmRepository farmRepository;
+
+  @Autowired
+  public FarmService(FarmRepository farmRepository) {
+    this.farmRepository = farmRepository;
+  }
+
+  public Farm createFarm(Farm farm) {
+    return farmRepository.save(farm);
+  }
+
+  public List<Farm> findAllFarms() {
+    return farmRepository.findAll();
+  }
+
+  public Farm findByFarmId(Long id) throws FarmNotFoundException {
+    return  farmRepository.findById(id)
+            .orElseThrow(FarmNotFoundException::new);
+  }
+
+  public Farm updateFarm(Farm farm) throws FarmNotFoundException {
+    Farm farmExisting = farmRepository.findById(farm.getId())
+            .orElseThrow(FarmNotFoundException::new);
+
+    farmExisting.setName(farm.getName());
+    farmExisting.setSize(farm.getSize());
+
+    return farmRepository.save(farmExisting);
+  }
+
+  public String deleteFarm (Long id) throws FarmNotFoundException {
+      Farm farmExisting = farmRepository.findById(id)
+              .orElseThrow(FarmNotFoundException::new);
+    farmRepository.delete(farmExisting);
+    return "Farm was successfully deleted.";
+  }
 }
